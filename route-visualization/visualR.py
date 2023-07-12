e772bc8396a923e3c0991d0cf94e70fd8b2ecf98
codusl100 <codusl100@naver.com>
2023-07-12 19:44:48 +0900

    refactor : 함수 변환

--- route-visualization/visualR.py
+++ route-visualization/visualR.py
@@ -1,3 +1,4 @@
+import random
 import urllib
 import folium
 import pandas as pd
@@ -7,61 +8,66 @@ import polyline
 
 osrm.RequestConfig.host = "router.project-osrm.org"
 
-Schedule = pd.read_csv('MILP.csv')
-Vehicles = np.sort(np.unique(Schedule['VID']))
 
 ColSets = ['#1B9E77', '#D95F02', '#7570B3', '#E7298A', '#66A61E', '#E6AB02', '#A6761D', '#666666']
 
-for vid in Vehicles:
-    Focused = Schedule[Schedule['VID'] == vid]
-    First = Focused.head(1)
-    Last = Focused.tail(1)
+def visualize(csv_file, color):
+    Schedule = pd.read_csv(csv_file)
+    Vehicles = np.sort(np.unique(Schedule['VID']))
 
-    waypts = [[0, First["From_Lon"].values[0], First["From_Lat"].values[0]]]
-    focus_list = Focused.index.to_list()
+    for vid in Vehicles:
+        Focused = Schedule[Schedule['VID'] == vid]
+        First = Focused.head(1)
+        Last = Focused.tail(1)
 
-    for row in range(focus_list[0], focus_list[-1]):
-        via = (row, Focused.loc[row, 'To_Lon'], Focused.loc[row, 'To_Lat'])
-        waypts.append(via)
+        waypts = [[0, First["From_Lon"].values[0], First["From_Lat"].values[0]]]
+        focus_list = Focused.index.to_list()
 
-    # 데이터프레임 생성
-    df = pd.DataFrame(waypts, columns=["com", "lon", "lat"])
+        for row in range(focus_list[0], focus_list[-1]):
+            via = (row, Focused.loc[row, 'To_Lon'], Focused.loc[row, 'To_Lat'])
+            waypts.append(via)
 
-    # 위도 경도 값 리스트 생성
-    try:
-        trips = osrm.trip(df[["lon", "lat"]].values.tolist(), overview="full")
-        print(trips)
-    # 400 에러 예외 처리
-    except urllib.error.HTTPError as e:
-        print(e.code)
-        print(e.read())
+        # 데이터프레임 생성
+        df = pd.DataFrame(waypts, columns=["com", "lon", "lat"])
 
-    trip = trips['trips'][0]['geometry']
-    route = polyline.decode(trip)
+        # 위도 경도 값 리스트 생성
+        try:
+            trips = osrm.trip(df[["lon", "lat"]].values.tolist(), overview="full")
+            print(trips)
+        # 400 에러 예외 처리
+        except urllib.error.HTTPError as e:
+            print(e.code)
+            print(e.read())
 
-    # 경로 시작 지점 & 종료 지점 & 거리
-    start_point = [trips['waypoints'][0]['location'][1], trips['waypoints'][0]['location'][0]]
-    end_point = [trips['waypoints'][1]['location'][1], trips['waypoints'][1]['location'][0]]
-    distance = trips['trips'][0]['distance']
+        trip = trips['trips'][0]['geometry']
+        route = polyline.decode(trip)
 
-    ps = Focused[Focused['Task_Type'] == "Pickup"]
-    ds = Focused[Focused['Task_Type'] == "Delivery"]
+        # 경로 시작 지점 & 종료 지점 & 거리
+        start_point = [trips['waypoints'][0]['location'][1], trips['waypoints'][0]['location'][0]]
+        end_point = [trips['waypoints'][1]['location'][1], trips['waypoints'][1]['location'][0]]
+        distance = trips['trips'][0]['distance']
 
-    MapResult = folium.Map(location=[(start_point[0] + end_point[0]) / 2, (start_point[1] + end_point[1]) / 2],
-                           zoom_start=13)
+        ps = Focused[Focused['Task_Type'] == "Pickup"]
+        ds = Focused[Focused['Task_Type'] == "Delivery"]
 
-    folium.TileLayer(tiles="CartoDB positron").add_to(MapResult)
-    folium.PolyLine(locations=route, color=ColSets[vid]).add_to(MapResult)
+        MapResult = folium.Map(location=[(start_point[0] + end_point[0]) / 2, (start_point[1] + end_point[1]) / 2],
+                               zoom_start=13)
 
-    for index, row in ps.iterrows():
-        # popup = f"{df['com'][index]}-{vid}"
-        folium.CircleMarker(
-            location=[row['To_Lat'], row['To_Lon']],
-            color='red',
-            stroke=True,
-            radius=3,
-            fill_opacity=0.8
-        ).add_to(MapResult)
-        print(row['To_Lat'], row['To_Lon'])
+        folium.TileLayer(tiles="CartoDB positron").add_to(MapResult)
+        folium.PolyLine(locations=route, color=color).add_to(MapResult)
 
-MapResult.save("Map.html")
+        for index, row in ps.iterrows():
+            # popup = f"{df['com'][index]}-{vid}"
+            folium.CircleMarker(
+                location=[row['To_Lat'], row['To_Lon']],
+                color='red',
+                stroke=True,
+                radius=3,
+                fill_opacity=0.8
+            ).add_to(MapResult)
+            print(row['To_Lat'], row['To_Lon'])
+
+    MapResult.save("Map.html")
+
+
+visualize('MILP.csv', random.choice(ColSets))
