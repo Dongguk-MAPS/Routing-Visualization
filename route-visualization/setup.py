508ab506421904183da1b9084a5473f2e298f0a2
codusl100 <codusl100@naver.com>
2023-07-12 19:05:21 +0900

    docs : setup.py 파일 추가

---
+++ route-visualization/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='route-visualization',
+    version='0.0.1',
+    description='Visualize map paths implemented with Python',
+    author='codusl100',
+    author_email='codusl100@naver.com',
+    url='https://github.com/Dongguk-MAPS/Routing-Visualization',
+    install_requires=['urllib', 'folium', 'pandas', 'numpy', 'osrm', 'polyline'],
+    packages=find_packages(exclude=[]),
+    keywords=['codusl100', 'MAPS', 'route visualization', 'python route visualization'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
